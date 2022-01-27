313c92462bf8249d7763b120f7126ed493b29412
Kayque <lkcoelho76@gmail.com>
2022-01-27 09:27:51 -0300

    integrate mongodb with nodejs in participants routes

--- src/app.js
+++ src/app.js
@@ -9,11 +9,36 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-app.post("/participants", (req, res) => {});
+app.post("/participants", async (req, res) => {
+  const participant = req.body;
+  try {
+    const mongoClient = await new MongoClient(process.env.Mongo_URI).connect();
+    const db = mongoClient.db("bate-papo-uol");
+    await db.collection("participants").insertOne(participant);
+
+    res.sendStatus(201);
+    mongoClient.close();
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
 app.post("/messages", (req, res) => {});
 app.post("/status", (req, res) => {});
 
-app.get("/participants", (req, res) => {});
+app.get("/participants", async (req, res) => {
+  try {
+    const mongoClient = await new MongoClient(process.env.Mongo_URI).connect();
+    const db = mongoClient.db("bate-papo-uol");
+    const participants = await db.collection("participants").find({}).toArray();
+
+    res.send(participants);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 app.get("/messages", (req, res) => {});
 
 app.listen(4000, () => "Server is listening on door 4000");
